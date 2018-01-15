0adab6c7ccea58e67beb98ba551a151c8f63e7e9
ikramart <ikramartt@gmail.com>
2018-01-15 15:07:41 +0800

    first commit

---
+++ app/src/main/java/com/ikramdev/videotube/Config.java
@@ -0,0 +1,14 @@
+package com.ikramdev.videotube;
+
+/**
+ key API: AIzaSyDXOQQJj03bdUfeckqjHLk3n2EtnN3j5mw
+ */
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyDXOQQJj03bdUfeckqjHLk3n2EtnN3j5mw";
+
+}
